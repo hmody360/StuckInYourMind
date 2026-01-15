ec360b55bb206399b3dd2c306b20f228735b470c
reef-a <reef.a.albadrani@gmail.com>
2026-01-15 20:34:28 +0300

    MovingAndDisappearingPlatforms

---
+++ Assets/_Reef/Scripts/Disappearing.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Disappearing : MonoBehaviour
+{
+    public float respawnTime = 3f;
+
+    Collider col;
+    MeshRenderer mesh;
+    bool used = false;
+
+    void Start()
+    {
+        col = GetComponent<Collider>();
+        mesh = GetComponent<MeshRenderer>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && !used)
+        {
+            used = true;
+            StartCoroutine(DisappearAndReturn());
+        }
+    }
+
+    IEnumerator DisappearAndReturn()
+    {
+        // تختفي فورًا
+        mesh.enabled = false;
+        col.enabled = false;
+
+        yield return new WaitForSeconds(respawnTime);
+
+        mesh.enabled = true;
+        col.enabled = true;
+        used = false;
+    }
+}
